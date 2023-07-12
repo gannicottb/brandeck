d2bb41817d8990611d9354e850b85ca5b1cabdc1
Brandon Gannicott <brandon.gannicott@gmail.com>
2023-07-12 15:05:14 -0500

    cleanup

--- components/astromon/v7/Mon.tsx
+++ components/astromon/v7/Mon.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { ParsedCard } from "lib/astromon/parse";
-import { icon, iconCircled } from "lib/astromon/utils";
+import { iconCircled } from "lib/astromon/utils";
 import cardStyles from 'styles/astromon/v7/Card.module.scss'
-import styles from 'styles/astromon/v7/Mon.module.scss'
 import { biomeColors } from "./Card";
 
 interface CardProps {
@@ -14,18 +13,7 @@ export const Mon: React.FC<CardProps> = ({ data, size, ...props }) => {
   const biomes = data.biome.split("")
   const colors = biomes.map(b => biomeColors[b])
   const biomeColor = colors.length == 1 ? colors[0] : `linear-gradient(to right, ${colors.join(",")})`
-  const biomeIcons = biomes.map(b => icon(b))
-  const [statType, statValue] = [data.skillType, data.skillValue]
-
-  const biomeAndSkill = (style: string) =>
-    <div className={style}
-      style={{ background: biomeColor }}
-    >
-      <div className={styles.mask}>
-        <div dangerouslySetInnerHTML={{ __html: biomeIcons.join("") }} />
-        <div dangerouslySetInnerHTML={{ __html: icon(statType) }} />
-      </div>
-    </div>
+  const statType = data.skillType
 
   const cornerOverlay =
     <div className={cardStyles.overlay} style={{ "fontSize": "2em" }}>
@@ -45,12 +33,10 @@ export const Mon: React.FC<CardProps> = ({ data, size, ...props }) => {
     >
       {cornerOverlay}
       <div className={cardStyles.topbar}>
-        {/* {biomeAndSkill(styles.biomeAndSkillLeft)} */}
         <div>
           <div className={cardStyles.name}>{data.name}</div>
           <div className={cardStyles.type}>{data.type}</div>
         </div>
-        {/* {biomeAndSkill(styles.biomeAndSkillRight)} */}
       </div>
       <div className={cardStyles.art}>
         {data.art != "unknown" && <img
@@ -62,12 +48,6 @@ export const Mon: React.FC<CardProps> = ({ data, size, ...props }) => {
       <div className={cardStyles.text}
         dangerouslySetInnerHTML={{ __html: data.text }}
       ></div>
-
-      <div className={styles.bottombar}>
-        {/* <div className={styles.stat}>
-          <div dangerouslySetInnerHTML={{ __html: iconCircled(statType) }} />
-        </div> */}
-      </div>
     </div >
   )
 }
