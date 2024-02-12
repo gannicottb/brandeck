50047c656f1aafd0c57ccafded77ba51d158e017
Brandon Gannicott <brandon.gannicott@gmail.com>
2024-02-12 00:07:10 -0600

    clean up lab template

--- src/_games/potions/v1/templates/Lab.tsx
+++ src/_games/potions/v1/templates/Lab.tsx
@@ -3,6 +3,8 @@ import { CardData } from "../parse";
 import Image from "next/image"
 
 export default function Lab({ data }: { data: CardData }) {
+  const topAndBottom = "flex justify-around w-[40%] ml-auto mr-auto text-2xl bg-orange-400 rounded-2xl"
+  const leftAndRight = "flex flex-col justify-center h-[30%] min-w-[18%] mt-auto mb-auto text-2xl bg-orange-400 rounded-2xl"
   return (
     <div className="flex flex-col h-full">
       <div className={`absolute top-0 w-full h-full m-0 -z-10`}>
@@ -15,18 +17,18 @@ export default function Lab({ data }: { data: CardData }) {
           sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"}
         />
       </div>
-      <div className="flex justify-around text-2xl bg-orange-400">
+      <div className={topAndBottom}>
         <MarkdownWithIcons content={data.upgrade1} />
       </div>
       <div className="flex h-full justify-between">
-        <div className="flex flex-col justify-center min-w-[18%] text-2xl bg-orange-400">
+        <div className={leftAndRight}>
           <MarkdownWithIcons content={data.upgrade2} />
         </div>
-        <div className="flex flex-col justify-center min-w-[18%] text-2xl bg-orange-400">
+        <div className={leftAndRight}>
           <MarkdownWithIcons content={data.upgrade3} />
         </div>
       </div>
-      <div className="flex justify-around text-2xl bg-orange-400">
+      <div className={topAndBottom}>
         <MarkdownWithIcons content={data.upgrade4} />
       </div>
     </div>
