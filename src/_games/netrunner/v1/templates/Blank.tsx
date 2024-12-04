5bacaffc9dfae3e0cb7ca57ceb48acf0be249e54
Brandon Gannicott <brandon.gannicott@gmail.com>
2024-12-03 23:02:36 -0600

    fix a bug of some kind, worked for HOL5

--- src/_games/netrunner/v1/templates/Blank.tsx
+++ src/_games/netrunner/v1/templates/Blank.tsx
@@ -2,12 +2,11 @@ import { CardData } from "../parse";
 import Image from "next/image"
 
 export default function Blank({ data }: { data: CardData }) {
-  console.log(data.art)
   return <div>
     <Image
       src={data.art}
       fill={true}
-      alt="art"
+      alt={data.name}
       referrerPolicy="no-referrer"
       priority
       sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"}
